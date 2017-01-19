0334e76c8254adcb0d0adc87c913b8e197715681
agencja-acclaim <hello@acclaim.pl>
2017-01-19 14:35:56 +0100

    Totally new structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 var gulp = require('gulp'),
+    sass = require('gulp-sass')
     fileinclude = require('gulp-file-include'),
     del = require('del');
 
@@ -23,7 +24,27 @@ var gulp = require('gulp'),
     });
 
 
-    /* ~~~~~~~~~~ Clean styles, scripts, and images ~~~~~~~~~~ */
+    /* ~~~~~~~~~~ Clone assets ~~~~~~~~~~ */
+
+    gulp.task('clone-assets',function(){
+        return gulp.src([
+            'images/**/*',
+            'pdf-files/**/*'
+        ],    {base: '.'})
+        .pipe(gulp.dest('dist/'));
+    });
+
+
+    /* ~~~~~~~~~~ Sass compiliation ~~~~~~~~~~ */
+
+    gulp.task('sass', function () {
+        return gulp.src('together-we-rock-styles.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('dist/'));
+    });
+
+
+    /* ~~~~~~~~~~ Clean dist folder ~~~~~~~~~~ */
 
     gulp.task('clean', function() {
         return del(['dist']);
@@ -37,5 +58,5 @@ var gulp = require('gulp'),
     /* ~~~~~~~~~~ Default task ~~~~~~~~~~ */
 
     gulp.task('default', ['clean'], function() {
-        gulp.start('fileinclude');
+        gulp.start('sass', 'clone-assets', 'fileinclude');
     });
